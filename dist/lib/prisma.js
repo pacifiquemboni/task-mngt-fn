e8fa5bdb4331ef6b9e333fc1b80438a5a411084a
pacifiquemboni <pacifiquemboni@gmail.com>
2026-03-25 16:40:22 +0200

    feat: add build and postbuild scripts, enhance deleteTag validation, and implement copy-generated script

--- dist/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../generated/prisma");
-const adapter_pg_1 = require("@prisma/adapter-pg");
-const adapter = new adapter_pg_1.PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new prisma_1.PrismaClient({ adapter });
-exports.default = prisma;
